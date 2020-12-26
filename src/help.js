ffa691989832a1589c14401ff209b1483761d5c6
man-x199 <74522163+man-x199@users.noreply.github.com>
2020-12-26 20:19:43 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,66 @@
+const help = (prefix) => {
+	return `>𝙋𝙪𝙙𝙞𝙙𝙞𝙓𝘽𝙤𝙏<
+
+*Status* : ```Online```
+*Groups* : ```13```
+*User* : ```27```
+*Fitur* : ```36```
+*Prefix* : (!)
+
+┏━━━°☛ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ☚°━━━┓
+┃
+┏➣ *${prefix}owner*
+┣➣ *${prefix}donasi*
+┗➣ *${prefix}creator*
+┃
+┣━━━°☛ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ☚°━━━⊱
+┃
+┣➣ *${prefix}sticker* [foto]
+┣➣ *${prefix}stickergif* [foto]
+┣➣ *${prefix}sticker nobg*
+┣➣ *${prefix}thunder* [teks]
+┣➣ *${prefix}tsticker* [teks/url]
+┃
+┣━━━━°☛ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ☚°━━━⊱
+┃
+┣➣ *${prefix}tts* [teks]
+┣➣ *${prefix}ocr*
+┣➣ *${prefix}loli*
+┣➣ *${prefix}toimg*
+┣➣ *${prefix}meme*
+┣➣ *${prefix}memeindo*
+┣➣ *${prefix}nsfwloli*
+┣➣ *${prefix}wait* [foto]
+┣➣ *${prefix}simi*
+┣➣ *${prefix}simih*
+┣➣ *${prefix}wait*
+┃
+┣━━━━°☛ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ☚°━━━━⊱
+┃
+┣➣ *${prefix}setname*
+┣➣ *${prefix}setdesc*
+┣➣ *${prefix}getpp*
+┣➣ *${prefix}tagall*
+┣➣ *${prefix}linkgroup*
+┣➣ *${prefix}gprofile*
+┣➣ *${prefix}setprefix*
+┣➣ *${prefix}welcome*
+┣➣ *${prefix}left*
+┃
+┣━━━━°☛ ❬ 𝙎𝙊𝙐𝙉𝘿 ❭ ☚°━━━━━⊱
+┃
+┣➣ *salam*
+┣➣ *tariksis*
+┣➣ *baka*
+┣➣ *desah*
+┣➣ *goblok*
+┣➣ *roti*
+┣➣ *welot*
+┣➣ *abangjago*
+┣━━━━━━━━━━━━━━━━━━━━
+┃🔹𝙏𝙃𝙄𝙎 𝘽𝙊𝙏 𝘽𝙔 𝘼𝙍𝙈𝘼𝙉𝙏𝙊𝘿🔹
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.help = help
+
